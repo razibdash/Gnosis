044fdf34c5c5412dcac1f35feb668e528ed71149
emranCoder <shaheyalam74@gmail.com>
2023-04-13 22:59:52 +0600

    favicon added

--- header.php
+++ header.php
@@ -2,11 +2,11 @@
 <html lang="en">
   <head>
     <title>LMS</title>
+    <link rel="icon" type="image/x-icon" href="image/favicon.png">
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" 
     integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
