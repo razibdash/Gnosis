bedb80e5229bd0dc67588c4c991fe6e63eda4cdd
avronilrajib <avronilrajib@gmail.com>
2023-04-15 14:29:08 +0600

    code change korci

--- sing-up.php
+++ sing-up.php
@@ -7,7 +7,7 @@
    <title>sing up</title>
    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" 
    integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-   <link rel="stylesheet" href="./css/student_info.css">
+   <link rel="stylesheet" href="style.css">
 </head>
 <body>
    <div class="sing-up-form student_info">
